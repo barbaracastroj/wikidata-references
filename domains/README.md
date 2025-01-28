2b106a50ab77614073bc7072f0bce4268d73c172
barbara <barbara.castro.j@gmail.com>
2025-01-27 21:04:37 -0300

    add script to sort domains

---
+++ domains/README.md
@@ -0,0 +1,9 @@
+# Obtención de lista de sitios web a descargar desde CommonCrawl
+
+El objetivo de este script es obtener una lista de sitios web para descargar los documentos de esos sitios desde CommonCrawl, se basa en dos criterios: a) cantidad de veces que se referenció el sitio en Wikipedia y b si existen suficiente cantidad de documentos del sitio en CommonCrawl
+
+## Cantidad de referencias en Wikipedia
+
+En su trabajo _External References of English Wikipedia_, Paolo Curotto, extrajo todas las url
+
+## Sitios con más documentos en CommonCrawl
---
+++ domains/top_domain_sorter.py
@@ -0,0 +1,34 @@
+import csv
+import gzip
+import json
+import tldextract
+from collections import Counter
+
+input_file = "ref-wiki-en_urls.txt.gz"
+
+
+def sort_domains():
+    domains = []
+    domain_counter = Counter()
+
+    with gzip.open(input_file, "rt") as infile:
+        for line in infile:
+            url = line.strip()
+            extracted = tldextract.extract(url)
+            registered_domain = f"{extracted.domain}.{extracted.suffix}"
+            domains.append(registered_domain)
+            domain_counter[registered_domain] += 1
+
+    sorted_domains = domain_counter.most_common()
+    domain_dict = dict(sorted_domains)
+
+    with open("top_domains.json", "w") as json_file:
+        json.dump(domain_dict, json_file, indent=4)
+
+    with open("top_domains.txt", "w") as txt_file:
+        for domain, _ in domain_dict.items():
+            txt_file.write(f"{domain}\n")
+
+
+if __name__ == "__main__":
+    sort_domains()
